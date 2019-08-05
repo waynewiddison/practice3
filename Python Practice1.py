fbefdb04ac942398d26adc8532abbf14de1308c1
CCEClass1 <CCEClass1@ad.weber.edu>
2019-08-05 10:32:23 -0600

    Initial commit

---
+++ Python Practice1.py
@@ -0,0 +1,27 @@
+print ("hello python")
+print ("hello")
+
+# None, and boolean
+print ("Binary notation", ob100)
+print ("Hex notation", 0x0100)
+
+
+# use the int () constructor
+# option for base
+print (int("1001", 2))
+
+# Floating point: IEEE-754
+# Scientific Notation
+print ("scientific notation", 1.61e-35)
+print ("Float construction float()", float ("1.99"))
+# Boolean
+print ("Boolean const. bool()", bool(0))
+print ("Boolean const. bool(0.1))", bool(0.1))
+# Test boolean on Collection
+#  I didn't add any text here.
+
+# Relational Operators: ==, != <,>, >-, <=
+a=12
+print("a==12", a==12)
+print ("a != 12", a != 12)
+
