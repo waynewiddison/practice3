a37d3f6b5db2b6db104cc29682a0f357ee3b2ff1
waynewiddison <eagle7100@gmail.com>
2019-08-05 11:32:26 -0600

    again 3

---
+++ Strings.py
@@ -0,0 +1,23 @@
+# file name Strings
+"""
+Strings and collections
+a strng is a immutable sequence of Unicolde code-points
+"""
+print ('this is a string')
+print ("this is a string too")
+# concatenation and adjacent strings
+s1 = "First "
+s2 = "Second"
+print (s1 + s2)
+
+# multi-line strings and newlines
+s3 = """This is
+a multiline
+ string"""
+print (s3)
+s4 = "This is\na multi-line\nstring"
+print(s4)
+ # support for backslash
+ s5 = "A\\in a string"
+ print (s5)
+
