bd89bf430ba47047e757c4602f12112862753bed
waynewiddison <eagle7100@gmail.com>
2019-08-05 12:50:55 -0600

    again 3

--- Strings.py
+++ Strings.py
@@ -16,8 +16,24 @@ a multiline
  string"""
 print (s3)
 s4 = "This is\na multi-line\nstring"
-print(s4)
+print (s4)
  # support for backslash
- s5 = "A\\in a string"
- print (s5)
+
+s5 = "A\\in a string"
+print (s5)
+
+s6 = 'this is "wow'
+print (s6)
+
+# Raw Strings
+raw_string = r'c:User\Documents\Books'
+print (raw_string)
+
+# capitalize the string
+print (s, s.capitalize())
+
+
+# String as sequence
+s = "arrot"
+print ("s[4]", s[4], type (s))  # index notation: 0, 1, 2, etc
 
