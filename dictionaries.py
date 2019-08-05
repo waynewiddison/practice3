1d6042d8ad55892f2c3bf09bfeb57508cfdd6423
waynewiddison <eagle7100@gmail.com>
2019-08-05 14:01:33 -0600

    again 5

---
+++ dictionaries.py
@@ -0,0 +1,27 @@
+"""
+Learn dictonaries
+Diectionaries map keys to values.
+
+In some languages are knows as associative arrays, or hashes, maps
+
+Create them using {} containing a key-value pair.
+Retrive them by [key_value] in brackets.
+this keeps them all uniform.
+"""
+d = {'alice':'801-123-34-8988', 'pedro': '956-00-8966', 'john': '345-67-8999'}
+print(d, type(d))
+# Access one element
+# order in dictionaries doesn't matter  just remember KEY, VALUE
+print(d['pedro'])
+
+# Add members to the dictionary, of names-> grades
+roster = {} # Empty dictionary
+# define dictionary then within the loop, ask for name / grade
+# d[k] = v
+total = 0
+While total < 3
+name - input ("Enter a player's name")
+score = input ("Enter Score")
+roster name # I never copied all this
+Print Name
+
