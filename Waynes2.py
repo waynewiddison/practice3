93504ab60ba10273fd71cc3290b9e4c4a5c66a41
waynewiddison <eagle7100@gmail.com>
2019-08-05 11:06:36 -0600

    add if statement once

--- Waynes2.py
+++ Waynes2.py
@@ -8,4 +8,37 @@ if True: #four spaces is universal
 
 
 print ("Done")
-# now save and push this
+# now save and push this using VCS / Commit / ??? and push
+
+if bool ("eggs")\
+    print("Yes, why not")
+
+h =42
+if h> 50
+    print ("greater than 50")
+
+else:
+    print ("less than 50")
+
+#########
+print ("Done")
+
+if h> 50
+    print ("greater than 50")
+elif h<50: #Else if
+    print ("less than 50")
+else:
+    print ("Equals 50")
+
+#Flat is better than Nested
+
+if h> 50
+    print ("greater than 50")
+    if h > 100:
+        print ("grater than 100")
+elif h<50: #Else if
+    print ("less than 50")
+else:
+    print ("Equals 50")
+
+
