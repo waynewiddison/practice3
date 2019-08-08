44391d35c9ffec84d7631b37b7d5e62b679c7a65
CCEClass1 <Eagle7100@gmail.com>
2019-08-08 16:17:56 -0600

    Thursday's class: Commit 3

--- airtravel.py
+++ airtravel.py
@@ -3,6 +3,8 @@ Learn about <insert topic here>
 Use in conjunction with program airtravel
 We created the class "flight"
 Second, we will create class "Aircraft"
+Note: as the class session progressed, we coded then deleated numerous lines of code,
+  which is no longer shown here.
 """
 
 class Flight:
@@ -10,7 +12,7 @@ class Flight:
     A flight wih a particular passenger aircraft
     """
 
-    def __init__(self, number):
+    def __init__(self, number, aircraft):
         # Initializes Flight number
         # implementation details begin with an underscore '_'
         # Validate flight number: 5 characters long: Alpha Alpha Digit Digit Digit
@@ -29,6 +31,10 @@ class Flight:
         if not number[2:].isdigit():
             raise ValueError("Invalid route code {}".format(number))
         self._number = number       # one underscore means I'm creating a private variable
+        self._aircraft = aircraft
+
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + [{letter:None for letter in seats} for _ in rows]
 
 
     def number(self):
@@ -40,6 +46,48 @@ class Flight:
 
     def airline(self):
         return self._number[:2]
+
+    def allocate_seat(self, seat, passenger):
+        """
+        Allocate a seat to a passenger
+        :param seat: A seat designator such as '12C', '21F'
+        :param passenger: The passenger name
+        :return:
+        """
+        rows, seat_letter = self._aircraft.seating_plan()
+        letter = seat[-1] # we only care about the letter, so take the letter from the seat
+        if letter not in seat_letter:
+            raise ValueError("Invalid seat letter {}".format(letter))
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row {}".format(row_text))
+
+        if row not in rows:
+            raise ValueError("Invalid row number {}".format(row))
+
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} already occupied".format(seat))
+        self._seating[row][letter] = passenger
+
+    def num_available_seats(self):
+        return sum(sum(1 for s in row.values() if s is None)
+                   for row in self._seating
+                   if row is not None)
+
+    def make_boarding_class(self, card_printer):
+        for passenger, seat in sorted(self._passenger_seats()):
+            card_printer(passenger, seat, self.number(), self._aircraft.model())
+
+    def _passenger_seats(self):
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for letter in seat_letters:
+                passenger = self._seating[row][letter]
+                if passenger is not None:
+                    yield (passenger, "{} {}".format(row, letter))
 
 
 class Aircraft:
@@ -57,6 +105,12 @@ class Aircraft:
         return self._model
 
     # right here, I could create functions for rows, seats, and num of seats per row
+    # next create a function for more mapping including rows and seats
+
+    def seating_plan(self):
+        return(range(1, self._num_rows + 1),
+               "ABCDEFGHJK"[:self._num_seats_per_row])
+        # return (rows, seats);
 
 def main():
     """
@@ -64,7 +118,6 @@ def main():
     :return:
     """
     pass
-
 
 
 if __name__ == '__main__':
--- use_airtravel.py
+++ use_airtravel.py
@@ -5,6 +5,35 @@ Use in conjunction with program airtravel
 """
 
 from airtravel import Flight, Aircraft
+from pprint import pprint as pp
+  # pp is "pretty print" which prints in 'pretty' rows and columns
+
+def make_flight():
+    flight = Flight("SN066", Aircraft("G-EUP",
+                                  "Airbus A319",
+                                  num_rows=22,
+                                  num_seats_per_row=6))
+    # pp(f1._seating)
+    flight.allocate_seat("02A", "Wayne Widdison")
+    flight.allocate_seat("12B", "Rasmus Lerdorf")
+    flight.allocate_seat("15F", "Bjare Larsen")
+    flight.allocate_seat("03A", "Yakob Smirnoff")
+    flight.allocate_seat("16F", "Yukihiro Matsumoto")
+    return flight
+
+
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = "| Name: (0)" \
+             " Flight: (1) " \
+             " Seat: (2)" \
+             " Aircraft: (3)" \
+             " |".format(passenger, flight_number, seat, aircraft)
+    banner = "+" + "-" * (len(output) - 2) + "+"
+    border = "|" + " " * (len(output) - 2) + "|"
+    lines = [banner, border, output, border, banner]
+    card = '\n'.join(lines)
+    print(card)
+    print()
 
 
 def main():
@@ -12,17 +41,10 @@ def main():
     Test function
     :return:
     """
-    f1 = Flight("SN066")
-    print(f1.number(), f1.airline())
-    # print(f, type(f))
-    # print(f.number())
-    # f2 = Flight("ab345")
-    f2 = Flight("AB352")
-    print(f2.number(), f2.airline())
-    # could use: Flight.number(f)
-    a1 = Aircraft("G-EUP", "Airbus A319", num_rows = 22, num_seats_per_row = 6)
-    print(a1.registration(), a1.model())
-
+    flight_1 = make_flight()
+    pp(flight_1._seating)
+    print("Available seats", flight_1.num_available_seats())
+    flight_1.make_boarding_class(console_card_printer) # be careful, this has to be passed correctly
 
 
 if __name__ == '__main__':
